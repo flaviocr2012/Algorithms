28c6ead17d8824d45ec885358906d5ae094fb3bc
flaviocr2012 <flaviocr2012@gmail.com>
2023-10-09 20:50:44 -0300

    complexidade constante

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Complexidade O(1) Constante - Acesso direto a um elemento do Array:
+        int [] arr = {1,2,3,4,5};
+        int elemento = arr[2];
+
+        System.out.println(elemento);
+
+    }
+}
